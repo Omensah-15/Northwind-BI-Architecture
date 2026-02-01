64665f9ec99a6e2531d154615366ac63ab40b276
Mensah Obed <133921584+Omensah-15@users.noreply.github.com>
2026-02-01 16:51:16 +0000

    Update 06_Full Relationship Analysis.sql

--- queries/Joins & Relationships/06_Full Relationship Analysis.sql
+++ queries/Joins & Relationships/06_Full Relationship Analysis.sql
@@ -7,7 +7,6 @@ Immediate Revenue Opportunity: These customers are your most diverse, loyal, and
 They have the highest lifetime value and represent your best upsell/cross-sell targets for new products.
 
 Apraoch:
-
 */
 
 
